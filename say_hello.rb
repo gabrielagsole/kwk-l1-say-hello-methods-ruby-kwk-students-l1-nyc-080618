8195d493bec87226dbc386ac68fb80c27aa60dac
gabrielagsole <ggarciasole@gmail.com>
2018-08-07 14:50:15 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,15 +1,3 @@
-def say_hello (your_name)
-  puts "Hello #{your_name}!"
-end 
-
-say_hello ("Gabriela")
-
-def default_arguments (name = "Ruby programmer")
-  puts "Hello, #{name}"
-end
-
-default_arguments 
-
 def say_hello(your_name = "Ruby Programmer")
   puts "Hello #{your_name}"
 end
